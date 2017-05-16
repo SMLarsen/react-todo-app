6fea6a2ce0ebb9c17261cc61c3a86dee22a49fc0
Steve Larsen <stevelarsen01@gmail.com>
2017-05-16 14:34:40 -0500

    complete per youtube reqs

--- src/components/app.js
+++ src/components/app.js
@@ -26,11 +26,12 @@ export default class App extends React.Component {
 		return (
 			<div>
 				<h1>React Todo App</h1>
-        <CreateTodo createTask={this.createTask.bind(this)}/>
+        <CreateTodo todos={this.state.todos} createTask={this.createTask.bind(this)}/>
         < TodosList
           todos={this.state.todos}
           toggleTask={this.toggleTask.bind(this)}
-
+          saveTask={this.saveTask.bind(this)}
+          deleteTask={this.deleteTask.bind(this)}
           />
 			</div>
 			);
@@ -42,11 +43,22 @@ export default class App extends React.Component {
     this.setState({ todos: this.state.todos });
   }
 
+  saveTask(oldTask, newTask) {
+    const foundTodo = _.find(this.state.todos, todo => todo.task === oldTask);
+    foundTodo.task = newTask;
+    this.setState({ todos: this.state.todos });
+  }
+
   createTask(task) {
     this.state.todos.push({
       task,
       isCompleted: false
     });
+    this.setState({todos: this.state.todos});
+  }
+
+  deleteTask(taskToDelete) {
+    _.remove(this.state.todos, todo => todo.task === taskToDelete);
     this.setState({todos: this.state.todos});
   }
 }
--- src/components/todos-list-item.js
+++ src/components/todos-list-item.js
@@ -11,55 +11,107 @@ export default class TodosListItem extends React.Component {
   }
 
   renderTaskSection() {
-    const { task, isCompleted } = this.props;
+    const {
+      task,
+      isCompleted
+    } = this.props;
 
     const taskStyle = {
-        color: isCompleted ? 'green' : 'red',
-        cursor: 'pointer'
+      color: isCompleted ? 'green' : 'red',
+      cursor: 'pointer'
     };
 
-    return(
-      <td
-        style={taskStyle}
-        onClick={(this.props.toggleTask.bind(this, task))}
-        >
-        {task}
-        </td>
-    );
-  }
-
-    renderActionsSection() {
-      if (this.state.isEditing) {
-        return (
-          <td>
-            <button>Save</button>
-            <button onClick={this.onCancelClick.bind(this)}>Cancel</button>
-          </td>
-        );
-      }
-
-      return (
-        <td>
-          <button onClick={this.onEditClick.bind(this)}>Edit</button>
-          <button>Delete</button>
-        </td>
+    if (this.state.isEditing) {
+      return ( <
+        td >
+        <
+        form onSubmit = {
+          this.onSaveClick.bind(this)
+        } >
+        <
+        input type = "text"
+        defaultValue = {
+          task
+        }
+        ref = "editInput" / >
+        <
+        /form> <
+        /td>
       );
     }
 
-	render () {
-		return (
-      	<tr>
-          {this.renderTaskSection()}
-          {this.renderActionsSection()}
-      	</tr>
-			);
-	}
-
-  onEditClick() {
-      this.setState({ isEditing: true });
+    return ( <
+      td style = {
+        taskStyle
+      }
+      onClick = {
+        (this.props.toggleTask.bind(this, task))
+      } > {
+        task
+      } <
+      /td>
+    );
   }
 
-    onCancelClick() {
-        this.setState({ isEditing: false });
+  renderActionsSection() {
+    if (this.state.isEditing) {
+      return ( <
+        td >
+        <
+        button onClick = {
+          this.onSaveClick.bind(this)
+        } > Save < /button> <
+        button onClick = {
+          this.onCancelClick.bind(this)
+        } > Cancel < /button> < /
+        td >
+      );
     }
+
+    return ( <
+      td >
+      <
+      button onClick = {
+        this.onEditClick.bind(this)
+      } > Edit < /button> <
+      button onClick = {
+        this.props.deleteTask.bind(this, this.props.task)
+      } > Delete < /button> < /
+      td >
+    );
+  }
+
+  render() {
+    return ( <
+      tr > {
+        this.renderTaskSection()
+      } {
+        this.renderActionsSection()
+      } <
+      /tr>
+    );
+  }
+
+  onEditClick() {
+    this.setState({
+      isEditing: true
+    });
+  }
+
+  onSaveClick(event) {
+    event.preventDefault();
+
+    const oldTask = this.props.task;
+    const newTask = this.refs.editInput.value;
+    this.props.saveTask(oldTask, newTask);
+    this.setState({
+      isEditing: false
+    });
+  }
+
+  onCancelClick() {
+    this.setState({
+      isEditing: false
+    });
+  }
 }
